43b199ffced188d5aa0ebffc827b9a8365308e09
Tom Leonards <tom.leonards2003@gmail.com>
2024-01-10 23:01:12 +0100

    asdasd

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent{
+
+
+  isLoading: boolean = false;
   isLoggedIn: boolean = false;
+  
 
   login() {
-    this.isLoggedIn = true;
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoggedIn = true;
+    }, 3000); 
   }
+  
+
+
 }
