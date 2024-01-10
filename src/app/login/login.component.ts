cf3df576a624f614556fb586a53fcb48fb02ef2f
Tom Leonards <tom.leonards2003@gmail.com>
2024-01-10 18:14:04 +0100

    cahnge

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isLoggedIn: boolean = false;
 
+  login() {
+    this.isLoggedIn = true;
+  }
 }
