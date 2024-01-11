c99248fc39c1579b6b36f34603c00a553095bc75
Tom Leonards <tom.leonards2003@gmail.com>
2024-01-11 16:54:53 +0100

    add redikrectt

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,13 +10,19 @@ export class LoginComponent{
 
   isLoading: boolean = false;
   isLoggedIn: boolean = false;
+  isWrong: boolean = false;
   
 
   login() {
+    this.isWrong = true
     this.isLoading = true;
     setTimeout(() => {
       this.isLoggedIn = true;
     }, 3000); 
+    
+    setTimeout(() => {
+      window.location.href = 'https://www.xpnda.de';
+    }, 3500); 
   }
   
 
